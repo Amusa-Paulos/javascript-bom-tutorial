ddf2f7d6a60e16b5415faccd15fc977bb5828837
Hostel Rentie <hostelrentie@gmail.com>
2021-12-15 06:49:37 +0100

    javascript BOM screen explained

---
+++ javascript screen/index.js
@@ -0,0 +1,8 @@
+console.log(window.screen);
+console.log(screen);
+console.log(screen.width);
+console.log(screen.height);
+console.log(screen.availWidth);
+console.log(screen.availHeight);
+console.log(screen.orientation.angle);
+console.log(screen.orientation);
