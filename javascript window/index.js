16e0c0dff87f166f895ade3e3425b56f24717ec9
Hostel Rentie <hostelrentie@gmail.com>
2021-12-15 06:53:58 +0100

    Javascript window explained

---
+++ javascript window/index.js
@@ -0,0 +1,16 @@
+console.log(window)
+// document
+console.log(window.document);
+
+window.x = 'hello world'
+
+
+function greet (){
+    console.log("window.x", window.x);
+    console.log("x", x);
+}
+
+greet()
+
+// window.close()
+// window.open()
